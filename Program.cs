1d718f620617dca987a031e1f103e6ba4fd1f1ea
Dildora Nasriddinzoda <80986802+BlackStar0020@users.noreply.github.com>
2021-08-16 23:21:38 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,70 +10,101 @@ namespace _10_08_2021
     {
         static void Main(string[] args)
         {
-            //1
+                   ///////////    1
             double a = double.Parse(Console.ReadLine());
-             if (a > 500 &&  a<1000)
-             {
-                 double sk = (a * 3) / 100, stoimost3 = a - sk;
-                 Console.WriteLine($"{stoimost3} ");
+            if (a > 500 && a < 1000)
+            {
+                double sk = (a * 3) / 100, stoimost3 = a - sk;
+                Console.WriteLine($"{stoimost3} ");
 
-             }
-             else if (a > 1000)
-             {
-                 double ск = (a * 5) / 100, stoimost5 = a - сk;
-                 Console.WriteLine($"{stoimost5} ");
+            }
+            else if (a > 1000)
+            {
+                double ck = (a * 5) / 100, stoimost5 = a - ck;
+                Console.WriteLine($"{stoimost5} ");
 
-             }else
-             {
-                 Console.WriteLine($"{a}");
-             }
-             Console.ReadKey();
-            
-            //2
-             double a = double.Parse(Console.ReadLine());
-             double b = double.Parse(Console.ReadLine());
-             double c = double.Parse(Console.ReadLine());
-             double d = double.Parse(Console.ReadLine());
-             if (!(a < b && b < c && c < d))
-             {
-                 double min1 = Math.Min(a, b), min2 = Math.Min(c, d), min3 = Math.Min(sum1, sum2);
-                 Console.WriteLine($"{min3}");
-             } 
-             else if (a == b && b == c   && c == d)
-             {
-                 double proiz = a * b * c * d;
-                 Console.WriteLine($"{proiz}");
-             }
-             else
-             {
-                 Console.WriteLine("Числа расположены по возрастанию");
-             }
-             Console.ReadKey();
-            //3
+            }
+            else
+            {
+                Console.WriteLine($"{a}");
+            }
+            Console.ReadKey();
+
+          ////////  2
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
             double c = double.Parse(Console.ReadLine());
-            if(a == b && b == c && c == a)
+            double d = double.Parse(Console.ReadLine());
+            if (!(a <= b && b <= c && c <= d))
+            {
+                double sum1 = Math.Min(a, b), sum2 = Math.Min(c, d), sum3 = Math.Min(sum1, sum2);
+                Console.WriteLine($"{sum3}");
+            }
+            else if (!(a <= b && b <= c && c < d))
+            {
+                double sum1 = Math.Min(a, b), sum2 = Math.Min(c, d), sum3 = Math.Min(sum1, sum2);
+                Console.WriteLine($"{sum3}");
+            }
+            else if (!(a <= b && b < c && c < d))
+            {
+                double sum1 = Math.Min(a, b), sum2 = Math.Min(c, d), sum3 = Math.Min(sum1, sum2);
+                Console.WriteLine($"{sum3}");
+            }
+            else if (!(a <= b && b <= c && c < d))
+            {
+                double sum1 = Math.Min(a, b), sum2 = Math.Min(c, d), sum3 = Math.Min(sum1, sum2);
+                Console.WriteLine($"{sum3}");
+            }
+            else if (!(a < b && b <= c && c <= d))
+            {
+                double sum1 = Math.Min(a, b), sum2 = Math.Min(c, d), sum3 = Math.Min(sum1, sum2);
+                Console.WriteLine($"{sum3}");
+            }
+            else if (!(a <= b && b < c && c <= d))
+            {
+                double sum1 = Math.Min(a, b), sum2 = Math.Min(c, d), sum3 = Math.Min(sum1, sum2);
+                Console.WriteLine($"{sum3}");
+            }
+            else if (!(a < b && b < c && c <= d))
+            {
+                double sum1 = Math.Min(a, b), sum2 = Math.Min(c, d), sum3 = Math.Min(sum1, sum2);
+                Console.WriteLine($"{sum3}");
+            }
+            else if (a == b && b == c && c == d)
+            {
+                double proiz = a * b * c * d;
+                Console.WriteLine($"{proiz}");
+            }
+            else
+            {
+                Console.WriteLine("Числа расположены по возрастанию");
+            }
+            Console.ReadKey();
+          //////////  3
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            if (a == b && b == c && c == a)
             {
                 Console.WriteLine($"a={a}, b={b} c={c}");
             }
-           else if (a>=b && a >= c && b>=c)
+            else if (a >= b && a >= c && b >= c)
             {
-                Console.WriteLine($"a={a}, b={b} c={c}") ;
+                Console.WriteLine($"a={a}, b={b} c={c}");
             }
-           else if(b>=a && b >= c && a>=c)
+            else if (b >= a && b >= c && a >= c)
             {
                 Console.WriteLine($"a={b}, b={a} c={c}");
             }
-           else if(b>=c && b >= a && c>=a)
+            else if (b >= c && b >= a && c >= a)
             {
                 Console.WriteLine($"a={b}, b={c} c={a}");
             }
-            else if(c>=b && c>=a && b >= a)
+            else if (c >= b && c >= a && b >= a)
             {
                 Console.WriteLine($"a={c}, b={b} c={a}");
             }
-            else if(c >= b && c >= a && a >= b)
+            else if (c >= b && c >= a && a >= b)
             {
                 Console.WriteLine($"a={c}, b={a} c={b}");
             }
